affd50cfe4861de11571aa0420d5a673698c93ae
bwilcox <wilcox.benoit@gmail.com>
2015-02-27 09:52:46 +0100

    ** tech ** : License header

--- build-and-deploy/remote-deployment/deploy-qa.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-ansible-playbook -i inventory/qa site.yml
-# ansible-playbook -i hosts -l application site.yml
--- src/main/java/org/dockland/async/ExceptionHandlingAsyncTaskExecutor.java
+++ src/main/java/org/dockland/async/ExceptionHandlingAsyncTaskExecutor.java
@@ -1,13 +1,28 @@
+/**
+ *  Copyright (C) 2015  Dockhouse project org. ( http://dockhouse.github.io/ )
+ *
+ *  Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *          http://www.gnu.org/licenses/lgpl.html
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.dockland.async;
-
-import java.util.concurrent.Callable;
-import java.util.concurrent.Future;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.core.task.AsyncTaskExecutor;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
 
 public class ExceptionHandlingAsyncTaskExecutor implements AsyncTaskExecutor,
         InitializingBean, DisposableBean {
