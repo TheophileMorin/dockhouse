0299ea0923751b00f974a70e2e271c9fa93b488c
bwilcox <wilcox.benoit@gmail.com>
2015-03-14 21:28:20 +0100

    [Tech] : Init continuous integration

--- build-and-deploy/image/build-image.sh
+++ build-and-deploy/image/build-image.sh
@@ -8,12 +8,6 @@ echo 'Retrieve the current application version ...'
 APP_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version|grep -Ev '(^\[|Download\w+:)')
 echo "Version =  $APP_VERSION ..."
 
-# Run the packaging
-echo '#############################'
-echo '#  APPLICATION PACKAGING    #'
-echo '#############################'
-mvn -Pprod -DskipTests=true clean package
-
 echo '##########################'
 echo '#  DOCKER IMAGE BUILD    #'
 echo '##########################'
