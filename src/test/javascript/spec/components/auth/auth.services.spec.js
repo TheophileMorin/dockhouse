f34148156f2f98a637096017f5a6052f88276f47
Theophile Morin <theophile.morin.pro@gmail.com>
2015-05-05 23:32:18 +0200

    ** Bugfix ** : tests with breadcrumbs in the HTML.

--- src/test/javascript/spec/components/auth/auth.services.spec.js
+++ src/test/javascript/spec/components/auth/auth.services.spec.js
@@ -15,13 +15,16 @@ describe('Services Tests ', function () {
             //Request on app init
             $httpBackend.expectPOST(/api\/logout\?cacheBuster=\d+/).respond(200, '');
 
+
             $httpBackend.expectGET('i18n/en/global.json').respond(200, '');
             $httpBackend.expectGET('i18n/en/language.json').respond(200, '');
             $httpBackend.expectGET('scripts/components/navbar/navbar.html').respond({});
+            $httpBackend.expectGET('scripts/components/breadcrumbs/breadcrumbs.html').respond({});
             $httpBackend.expectGET('i18n/en/global.json').respond(200, '');
             $httpBackend.expectGET('i18n/en/language.json').respond(200, '');
             $httpBackend.expectGET('i18n/en/main.json').respond(200, '');
             $httpBackend.expectGET('scripts/app/main/main.html').respond({});
+
 
           }));
         //make sure no expectations were missed in your tests.
